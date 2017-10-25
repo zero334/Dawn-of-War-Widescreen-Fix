71727ca12a8c31e98ad9e59dc87dd3a61e7db567
Patrick Elsässer <paddy334@web.de>
2017-10-25 19:44:06 +0200

    Projektdateien hinzufügen.

---
+++ Dawn of War Widescreen Fix/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dawn_of_War_Widescreen_Fix
+{
+    static class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
